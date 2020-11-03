b1431a0196d92b8dd9c2e50dd59e92a401b1cfca
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-11-03 16:19:34 -0300

    :sparkles: Redirect after load

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-$(() => {
+function typeWord(element){
+    var letterArray = element.html().split('');
+    element.html('');
+    letterArray.forEach((letter, i)=> {
+        setTimeout(() => {
+            var str = element.html();
+            element.html(str += letter);
+            // console.log(element.html(str += letter));
+            // element.+= letter); 
+        }, 80 * i);    
+    });
+}
+var string = $('h1');
+typeWord(string);
 
-    function typeWord(element){
-        var letterArray = element.html().split('');
-        element.html('');
-        letterArray.forEach((letter, i)=> {
-            setTimeout(() => {
-                var str = element.html();
-                element.html(str += letter);
-                // console.log(element.html(str += letter));
-                // element.+= letter); 
-            }, 80 * i);    
-        });
-        
-    }
-    var string = $('h1');
-    typeWord(string);
-});
+setTimeout(() => {
+    window.location.href = 'https://github.com/lucasemanuel';
+}, 3000);    
+

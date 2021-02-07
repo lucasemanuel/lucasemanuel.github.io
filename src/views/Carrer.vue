cd95f7abcf9318c9a06c7b3259c20b220decc514
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-07 13:15:16 -0300

    Adicionando views

---
+++ src/views/Carrer.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <Section title="Carre" />
+  </section>
+</template>
+
+<script>
+import Section from "../components/Section.vue"
+
+export default {
+  name: "Carrer",
+  components: {
+    Section,
+  },
+}
+</script>
+
+<style></style>
---
+++ src/views/Project.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <Section title="Projetcs" />
+  </section>
+</template>
+
+<script>
+import Section from "../components/Section.vue"
+
+export default {
+  name: "Project",
+  components: {
+    Section,
+  },
+}
+</script>
+
+<style></style>

de4bc4331f762eb94e5a217ef7b4f2c9a53d1854
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-07 09:17:49 -0300

    Adicionando componente para conteúdo

---
+++ src/components/Section.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <header class="title">
+      <h3>{{ title }}</h3>
+      <hr>
+    </header>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Section",
+  props: {
+    title: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+section {
+  flex: 1;
+  text-align: start;
+}
+
+header hr {
+  margin-top: -14px;
+  height: 12px;
+  background-color: var(--main-color);
+}
+
+h3 {
+  margin-left: 24px;
+  font-size: 28px;
+}
+
+</style>

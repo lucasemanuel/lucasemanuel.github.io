0c6e369fecf20c2cc88a210d1bc7c598dfc849d2
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-02-07 09:18:42 -0300

    Adicionando visão 'Sobre'

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <Section title="About Me" />
+  </section>
+</template>
+
+<script>
+import Section from "../components/Section.vue"
+
+export default {
+  name: "About",
+  components: {
+    Section,
+  },
+}
+</script>
+
+<style></style>
